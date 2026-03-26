789ef302635208a13440ac08fad0d7b4101068d7
AKToran <aktoran60@gmail.com>
2026-03-26 18:39:23 +0600

    nav and footer done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FaFacebook, FaLinkedin, FaX } from 'react-icons/fa6';
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className='bg-blue-950 text-white p-4 lg:p-8'>
+      <div className='flex justify-between pb-4'>
+        <a href="/" className="flex items-center text-xl font-bold text-white">
+          <span>
+            <img className="w-6 lg:w-10" src="/logo.png" alt="" />
+          </span>
+          APP.CHAI
+        </a>
+        <div className='space-y-3'>
+          <h3 className='text-xl font-semibold'>Social Links</h3>
+          <div className='flex items-center space-x-2'>
+            <FaX/>
+            <FaLinkedin/>
+            <FaFacebook/>
+          </div>
+        </div>
+      </div>
+
+      <hr className='text-gray-400' />
+      <div className='pt-4 text-center'>
+        <p >Copyright © 2025 - All right reserved</p>
+      </div>
     </div>
   );
 };
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
+import "./Nav.css";
+import { FaGithub } from "react-icons/fa6";
 
 const Nav = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/apps"}>Apps</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/installation"}>Installation</NavLink>
+      </li>
+    </>
+  );
   return (
-    <div>
-      nav
+    <div className="navbar bg-base-100 shadow-sm px-0 lg:px-8">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a href="/" className="flex items-center text-xl font-bold text-blue-900">
+          <span>
+            <img className="w-6 lg:w-10" src="/logo.png" alt="" />
+          </span>
+          APP.CHAI
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal text-lg px-1 space-x-2 font-semibold">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end ">
+        <a className="btn bg-blue-900 text-white"> <span className="text-lg"><FaGithub/></span> Contribute</a>
+      </div>
     </div>
   );
 };
